57b2487356147c5632a7d078ff83e823e4f5f76c
Brian Jimerson <64101689+btjimerson@users.noreply.github.com>
2023-10-03 11:22:04 -0400

    Initial commit

---
+++ src/main/java/xyz/pintobean/json/JsonSampleApplication.java
@@ -0,0 +1,13 @@
+package xyz.pintobean.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonSampleApplication.class, args);
+	}
+
+}
