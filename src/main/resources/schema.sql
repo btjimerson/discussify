1fe30d0d36b189b61df4f94adbe277d5e89bb407
Brian Jimerson <bjimerson@gmail.com>
2023-10-04 17:43:33 -0400

    Add database init files

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE public.post (
+    id uuid NOT NULL,
+    author character varying(255),
+    comments jsonb,
+    content character varying(255),
+    post_date timestamp(6) without time zone,
+    title character varying(255)
+);
+
+ALTER TABLE ONLY public.post
+    ADD CONSTRAINT post_pkey PRIMARY KEY (id HASH);
+
